a8fc6676b5cb08a696166e388d089d4a93a5b7f6
LittleBottle <26377029+PingZi818@users.noreply.github.com>
2023-09-27 13:49:45 +0800

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+import { useAuth } from "context/auth-context";
+import { ErrorBoundary } from "components/error-boundary";
+import { FullPageErrorFallback, FullPageLoading } from "components/lib";
+
+const UnauthenticatedApp = React.lazy(() => import("unauthenticated-app"));
+
+function App() {
+  const { user } = useAuth();
+
+  return (
+    <div className="App">
+      {/* <ErrorBoundary fallbackRender={FullPageErrorFallback}> */}
+        <React.Suspense fallback={<FullPageLoading />}>
+          {user ? <div></div> : <UnauthenticatedApp />}
+        </React.Suspense>
+      {/* </ErrorBoundary> */}
+    </div>
+  );
+}
+
+export default App;
